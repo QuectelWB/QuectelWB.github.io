82fb74b8873bfca04968a85580cdc95ec593c21c
QuectelWB <panpanwang@aliyun.com>
2020-11-18 11:53:50 +0800

    2020年 11月 18日 星期三 11:53:47 CST

--- Others/C/flush.c
+++ Others/C/flush.c
@@ -1,15 +1,30 @@
 #include <stdio.h>  
 int main()  
 {  
-    char c;  
+    char c;
+
+    /***
     scanf("%c", &c);  
    printf("%d\n", c);  
 	
-fflush(stdin); // 冲掉马桶中的无用值 
+fflush(stdin); // 在GCC里无用
 
    scanf("%c", &c);  
     printf("%d\n", c);  
-	          
+
+*/
+
+        do {
+		c = getchar();
+	} while (c != '\n' && c != EOF);
+
+    printf("%d\n", c);  
+
+        do {
+		c = getchar();
+	} while (c != '\n' && c != EOF);
+
+    printf("%d\n", c);  
     return 0;  
 				  
 }  
---
+++ Others/gitlab.freedesktop.org.md
@@ -0,0 +1,18 @@
+gitlab.freedesktop.org
+========
+
+mmcli
+-----
+
+关于ModemManager 的源码
+
+git@gitlab.freedesktop.org:mobile-broadband/ModemManager.git
+
+https://gitlab.freedesktop.org/mobile-broadband/ModemManager.git
+
+Carl大神
+----
+
+[Quectel Carl](https://gitlab.freedesktop.org/carlyin)
+
+
