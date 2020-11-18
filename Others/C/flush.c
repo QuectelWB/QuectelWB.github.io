ca357977582f7c03a8925e60bc0f49f8dc3ccb81
QuectelWB <panpanwang@aliyun.com>
2020-11-18 10:04:37 +0800

    2020年 11月 18日 星期三 10:04:34 CST

---
+++ Others/C/flush.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+int main()  
+{  
+    char c;  
+    scanf("%c", &c);  
+   printf("%d\n", c);  
+	
+fflush(stdin); // 冲掉马桶中的无用值 
+
+   scanf("%c", &c);  
+    printf("%d\n", c);  
+	          
+    return 0;  
+				  
+}  
